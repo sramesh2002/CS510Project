71f7646549c6739511e5fb1fc0ffcaeb441d6bcf
Gautam Rao <gautamrao277@gmail.com>
2024-04-18 22:00:46 -0500

    post working

--- src/HealthWebApp/PredictorApp/urls.py
+++ src/HealthWebApp/PredictorApp/urls.py
@@ -3,4 +3,5 @@ from PredictorApp import views
 
 urlpatterns = [
     path("", views.home, name='home'),
+    path("/results", views.results, name='results'),
 ]
--- src/HealthWebApp/PredictorApp/views.py
+++ src/HealthWebApp/PredictorApp/views.py
@@ -1,5 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
     return render(request, "home.html", {})
+
+def results(request):
+    if request.method == 'POST':
+        age = request.POST.get('ageInput')
+        print(age)
+        conditions =  request.POST.get('conditionsTextarea')
+        race = request.POST.get('raceSelect')
+        gender = request.POST.get('gender', 'Not specified')
+
+        return render(request, 'results.html', {
+            'age': age,
+            'conditions': conditions,
+            'race': race,
+            'gender': gender
+        })
+    else:
+        return HttpResponse("Invalid request", status=400)
+
+
+        
+        
